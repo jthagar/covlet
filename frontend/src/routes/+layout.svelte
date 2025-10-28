704a457bc3b1f327cc37f78a35aff574b395b191
June Hagar <jaythagar@gmail.com>
2025-10-27 17:10:42 -0700

    add in current horrible setup

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import Header from './Header.svelte';
+	import Body from './Body.svelte';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Header />
+<Body />
+
+{@render children?.()}
---
+++ frontend/src/routes/Body.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Sidebar from './Sidebar.svelte';
+</script>
+
+<main class="flex w-screen bg-black text-white">
+	<div class="flex-[0.20] h-[90vh] overflow-y-hidden overflow-x-hidden gap-8">
+		<div class="flex flex-col pl-1 pt-4 md:ml-1 lg:ml-3 xl:ml-5 w-[75%] gap-4">
+			<Sidebar />
+		</div>
+	</div>
+	<div class="flex-[0.80] height-[90vh] overflow-y-scroll bg-gray-600">
+		<div class="flex flex-col pl-4 pt-4 md:ml-2 lg:ml-5 xl:ml-10 xl:mt-2 w-[90%] h-[90%] gap-4 bg-black text-white">
+			<!-- Main content-->
+			<h1>Combining SvelteKit and Golang server</h1>
+		</div>
+	</div>
+</main>
